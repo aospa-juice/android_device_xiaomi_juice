ce293099681e9889a7b1237300d90d9950da001b
xzyuan <xyzuannihboss@gmail.com>
2022-02-08 16:08:17 +0700

    juice: overlay-xd: Declare juice specs information
    - Also adapt card radius with our screen corner
    
    Signed-off-by: Enprytna <e.endipriyatna@gmail.com>

--- device.mk
+++ device.mk
@@ -389,7 +389,7 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-xd
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
