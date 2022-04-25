64ca5e061299ab2dd1197152dc8e47bf297e39e9
chrisw444 <wandersonrodriguesf1@gmail.com>
2022-04-25 19:20:29 +0000

    juice: Switch to vibrator component
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- device.mk
+++ device.mk
@@ -403,6 +403,9 @@ BENGAL := bengal
 TARGET_BOARD_PLATFORM := $(BENGAL)
 DEVICE := juice
 
+TARGET_COMMON_QTI_COMPONENTS := \
+    vibrator
+
 # QMI
 PRODUCT_PACKAGES += \
     libjson
@@ -502,9 +505,6 @@ PRODUCT_PACKAGES += \
     vndservicemanager
 
 # Vibrator
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
