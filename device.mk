aff12f38d4fabed4c404f9edaa9a7011b3bfd2b5
shelbyhell <lexaxx14@gmail.com>
2022-11-27 16:35:07 +0000

    Revert "juice: Clean display build packages"
    This reverts commit bb7da708175b57533731a5f1187349cfb231860e.

--- device.mk
+++ device.mk
@@ -159,8 +159,23 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Display
 PRODUCT_PACKAGES += \
-    android.frameworks.displayservice@1.0.vendor
-    
+    android.hardware.graphics.mapper@3.0-impl-qti-display \
+    android.hardware.graphics.mapper@4.0-impl-qti-display \
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service
+
+PRODUCT_PACKAGES += \
+    vendor.display.config@1.5 \
+    vendor.display.config@1.11.vendor \
+    vendor.display.config@2.0 \
+    vendor.display.config@2.0.vendor \
+    vendor.qti.hardware.display.allocator-service \
+    vendor.qti.hardware.display.composer-service \
+    vendor.qti.hardware.display.mapper@1.1.vendor \
+    vendor.qti.hardware.display.mapper@2.0.vendor \
+    vendor.qti.hardware.display.mapper@3.0.vendor \
+    vendor.qti.hardware.display.mapper@4.0.vendor
+
 PRODUCT_VENDOR_PROPERTIES += \
     debug.sf.enable_gl_backpressure=1 \
     vendor.display.idle_time=0 \
@@ -174,7 +189,12 @@ PRODUCT_SYSTEM_PROPERTIES += \
     debug.sf.latch_unsignaled=1
     
 PRODUCT_PACKAGES += \
-    libdisplayconfig.qti \
+    libdisplayconfig.qti
+
+PRODUCT_PACKAGES += \
+    libvulkan
+
+PRODUCT_PACKAGES += \
     disable_configstore
 
 # Dex2oat64
@@ -435,6 +455,9 @@ PRODUCT_COPY_FILES += \
 
 # WiFi Display
 PRODUCT_PACKAGES += \
+    libdisplayconfig.system.qti \
+    libqdMetaData.system \
+    libdisplayconfig.vendor \
     libwfdaac_vendor
 
 # Inherit the proprietary files
