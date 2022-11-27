af0046723b53cc92bed526358a2c5516729465e2
shelbyhell <lexaxx14@gmail.com>
2022-11-27 17:17:04 +0000

    juice: Cleanup.

--- device.mk
+++ device.mk
@@ -406,6 +406,9 @@ PRODUCT_VENDOR_PROPERTIES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
+    android.frameworks.sensorservice@1.0.vendor
+
+PRODUCT_PACKAGES += \
     android.hardware.sensors@2.0-service.multihal \
     libsensorndkbridge
 
